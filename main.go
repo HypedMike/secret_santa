7747ae0ae19dca44ee44d4fc0ec593b2956dbd4a
Mike <83963270+HypedMike@users.noreply.github.com>
2023-12-01 13:54:25 +0100

    improved performances

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const MODULE = 10
+
 func main() {
 	if os.Args[1] == "run" {
 		startSanta()
@@ -27,6 +29,9 @@ func decryptMyName(name string) {
 func startSanta() {
 	// load env file
 	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 
 	// read file
 	rawContent, err := os.ReadFile(os.Getenv("FILE_PATH"))
@@ -77,10 +82,16 @@ func startSanta() {
 	fmt.Println("------------------------------------------------------------")
 
 	// randomize matches
-	randomizeMatches(people)
+	randomizedPeople := randomizeMatches(people)
+
+	// print results
+	for _, person := range randomizedPeople {
+		fmt.Printf("%20s -> %20s\n", person.Name, person.Match)
+	}
+
 }
 
-func randomizeMatches(people []Person) {
+func randomizeMatches(people []Person) []CryptoMatch {
 	matches := make([]Person, len(people))
 
 	for i := 0; i < len(matches); i++ {
@@ -104,12 +115,31 @@ func randomizeMatches(people []Person) {
 		}
 	}
 
+	var matchesWithNames []CryptoMatch
+
 	// print results
 	for i := 1; i < len(matches); i++ {
-		fmt.Printf("%20s -> %20s\n", matches[i-1].Name, caesarEncrypt(matches[i].Name, 10))
+		matchesWithNames = append(matchesWithNames, CryptoMatch{
+			Name:  matches[i-1].Name,
+			Match: caesarEncrypt(matches[i].Name, MODULE),
+		})
 	}
 
-	fmt.Printf("%20s -> %20s\n", matches[len(matches)-1].Name, caesarEncrypt(matches[0].Name, 10))
+	matchesWithNames = append(matchesWithNames, CryptoMatch{
+		Name:  matches[len(matches)-1].Name,
+		Match: caesarEncrypt(matches[0].Name, MODULE),
+	})
+
+	return randomizeArray(matchesWithNames)
+
+	// fmt.Printf("%20s -> %20s\n", matches[len(matches)-1].Name, caesarEncrypt(matches[0].Name, 10))
+}
+
+func randomizeArray(input []CryptoMatch) []CryptoMatch {
+	rand.Shuffle(len(input), func(i, j int) {
+		input[i], input[j] = input[j], input[i]
+	})
+	return input
 }
 
 func findPerson(people []Person, name string) Person {
